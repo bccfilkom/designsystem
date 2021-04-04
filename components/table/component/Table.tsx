b745c62fe1b02399519aaed2ea4c1f91c8a910aa
muhajirmuhajir <hajir.nf@gmail.com>
2021-04-05 03:19:47 +0800

    refactor code

--- components/table/component/Table.tsx
+++ components/table/component/Table.tsx
@@ -1,26 +1,5 @@
 import * as React from "react";
-import styled from "styled-components";
-import { fonts } from "../../_utils/index";
-
-interface TableProps {
-  data: Data;
-  type?: boolean;
-  condensed?: boolean;
-  relaxed?: boolean;
-  grid?: boolean;
-  horizontalLine?: boolean;
-  stripe?: boolean;
-}
-
-interface Data {
-  header: HeaderItem[];
-  body: object[];
-}
-
-interface HeaderItem {
-  key: string | number;
-  name: string;
-}
+import {TableProps, Container, Td, Tr, Th} from "./components"
 
 export const Table: React.FC<TableProps> = ({
   data,
@@ -68,47 +47,3 @@ export const Table: React.FC<TableProps> = ({
   );
 };
 
-const Container = styled.table`
-  font-family: ${fonts};
-  min-width: 400px;
-  border-collapse: separate;
-  box-sizing: border-box;
-  overflow: hidden;
-  border-radius: 10px 10px 0 0;
-  border-spacing: 0;
-  border: 0.5px solid rgba(20, 48, 69, 0.05);
-`;
-
-const Th = styled.th`
-  ${(props) =>
-    props.horizontalLine
-      ? ""
-      : "border-left: 1px solid rgba(20, 48, 69, 0.05);"}
-  border-bottom: 1px solid rgba(20, 48, 69, 0.08);
-  text-align: left;
-  padding-top: 23px;
-  padding: 8px 16px;
-  box-sizing: border-box;
-`;
-
-const Tr = styled.tr`
-  &:nth-child(odd) {
-    ${(props) =>
-      props.stripe ? "background-color: rgba(185, 228, 251, 0.08);" : ""}
-  }
-`;
-
-const Td = styled.td`
-${(props) =>
-  props.horizontalLine
-    ? ""
-    : " border-left: 1px solid rgba(20, 48, 69, 0.05);"}
-  padding: ${(props) =>
-    props.condensed
-      ? "10px 16px 10px 16px"
-      : props.relaxed
-      ? "18px 16px 18px 16px"
-      : "14px 16px 14px 16px"};
-  box-sizing: border-box;
-  border-bottom: 1px solid rgba(20, 48, 69, 0.08);
-`;
