16e7484713218c922141a61799092b4ae74f8db9
Ibrahimsyah Zairussalam <ibra.himsyah10@gmail.com>
2021-08-09 22:10:01 +0700

    v1.0.3 release

--- components/switcher/index.ts
+++ components/switcher/index.ts
@@ -1,7 +1,2 @@
-<<<<<<< HEAD
 export { default as Switcher } from "./component/Switcher";
 export * from "./component/Switcher";
-=======
-export { default as Switcher } from './component/Switcher'
-export * from './component/Switcher'
->>>>>>> 7ba57b9a6f9c468b0b32b2e61d7b448bb89326b4
